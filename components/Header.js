080614096473dccae27ef89c5b6aceb7ee3eb21d
Haris Amjad <harisamjad0525@gmail.com>
2025-03-01 05:11:15 +0500

    absolute paths added

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,8 @@ export const Header = ({ seo }) => {
     name: `${seo.name}`,
     jobTitle:
       "Senior Software Engineer, Mobile Application Developer, React Native Developer",
-    image: `${seo.seo_img}`,
+    image:
+      "https://theharisamjad.com/_next/static/media/seo_thumbnail_haris.6074cb24.png",
     url: `${seo.url}`,
     sameAs: [
       "https://github.com/theharisamjad",
@@ -50,14 +51,22 @@ export const Header = ({ seo }) => {
       <meta name="robots" content="index, follow" />
       <meta property="og:title" content={seo.title} />
       <meta property="og:description" content={seo.description} />
-      <meta property="og:image" content={seo.og_img} />
+      <meta
+        property="og:image"
+        content="https://theharisamjad.com/_next/static/media/og_image_haris.6074cb24.png?v=2"
+      />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://theharisamjad.com" />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={seo.title} />
       <meta name="twitter:description" content={seo.description} />
-      <meta name="twitter:image" content={seo.twitter_img} />
+      <meta
+        name="twitter:image"
+        content="https://theharisamjad.com/_next/static/media/twitter_image_haris.6074cb24.png"
+      />
       <meta name="twitter:url" content="https://theharisamjad.com" />
     </Head>
   );
