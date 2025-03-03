ec811a8388124e349d10e0e31f1429507f9649c6
Haris Amjad <harisamjad0525@gmail.com>
2025-03-03 05:18:07 +0500

    fixed margin for mobile view

--- components/Intro.js
+++ components/Intro.js
@@ -8,9 +8,9 @@ export const Intro = ({ title, description, image, buttons }) => {
   return (
     <div className="bg-primary min-vh-100 d-flex align-items-center py-5 px-4 px-md-5">
       <div className="container">
-        <div className="row align-items-center text-center text-md-start g-5">
+        <div className="row align-items-center text-justify text-md-start g-2">
           {" "}
-          <div className="col-12 col-md-6 mb-2 mb-md-0 px-3">
+          <div className="col-12 col-md-6 mb-3 mb-md-0 px-3">
             {" "}
             <h1 className="text-secondary fw-bold display-3 mb-4">
               {title}
